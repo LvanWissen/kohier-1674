983dec9008660ab6210ed375ce4819dc6fc61183
Leon van Wissen <leon@vwissen.nl>
2019-12-06 17:27:12 +0100

    feat: conversion tot ROAR

---
+++ 2rdf.py
@@ -0,0 +1,135 @@
+from itertools import count
+
+import pandas as pd
+
+from rdflib import Graph, URIRef, Namespace, Literal
+
+from roar import rdfSubject
+from roar import Agent, Activity, Derivation
+from roar import Document, Annotation, PersonObservation, PersonName, LocationObservation, ResourceSelection, FragmentSelector, TextualBody, Aggregation, WebResource
+from roar import edm, roar, dc, oa, prov, dcterms, pnv
+
+personCounter = count(1)
+nsPersonObservation = Namespace(
+    "https://data.create.humanities.uva.nl/datasets/kohier1674/observations/PersonObservation/"
+)
+nsLocationObservation = Namespace(
+    "https://data.create.humanities.uva.nl/datasets/kohier1674/observations/LocationObservation/"
+)
+nsOccupationObservation = Namespace(
+    "https://data.create.humanities.uva.nl/datasets/kohier1674/observations/OccupationObservation/"
+)
+
+AGENT = Agent(URIRef("https://www.leonvanwissen.nl/#me"),
+              label=["Leon van Wissen"])
+ACTIVITY = Activity(
+    None,
+    wasAssociatedWith=[AGENT],
+    label=[Literal("OCR en correctie", lang='nl')],
+    comment=[
+        Literal(
+            "De scans van inventarisnummer 5028:622A zijn geOCRd met Tesseract (5.0.0-alpha) en daarna semi-automatisch gecorrigeerd.",
+            lang='nl')
+    ])
+DERIVATION = Derivation(None, hadActivity=ACTIVITY)
+
+
+def main(csvfile):
+
+    df = pd.read_csv(csvfile)
+    df = df.where((pd.notnull(df)), None)
+
+    g = rdfSubject.db = Graph(
+        identifier="https://data.create.humanities.uva.nl/datasets/kohier1674/"
+    )
+
+    g.bind('edm', edm)
+    g.bind('pnv', pnv)
+    g.bind('roar', roar)
+    g.bind('dc', dc)
+    g.bind('dcterms', dcterms)
+    g.bind('oa', oa)
+    g.bind('prov', prov)
+
+    images = set()
+    for r in df.to_dict(orient='records'):
+
+        scanName, _ = r['source'].split('_')
+        pageNumber = int(scanName[-5:])
+
+        document = Document(URIRef(f"#p{pageNumber}"))  #ProvidedCHO
+
+        image = URIRef(r['source'].replace('txt', 'tif'))
+        images.add(image)
+
+        aggregation = Aggregation(URIRef(
+            f"https://archief.amsterdam/archief/5028/622#p{pageNumber}"),
+                                  aggregatedCHO=document)
+
+        webResource = WebResource(
+            URIRef(f"https://archief.amsterdam/archief/5028/622A#{scanName}"))
+
+        aggregation.isShownAt = webResource
+
+        annotation = Annotation(
+            None,
+            hasTarget=ResourceSelection(
+                None,
+                hasSource=document,
+                hasSelector=FragmentSelector(
+                    None,
+                    conformsTo=URIRef("http://tools.ietf.org/rfc/rfc5147"),
+                    value=f"line={r['lineNumber']}")),
+            hasBody=TextualBody(None,
+                                value=r['reference'],
+                                language="nl",
+                                format="text/html"),
+            wasDerivedFrom=[image],
+            qualifiedDerivation=DERIVATION)
+
+        personNames = [
+            PersonName(
+                None,
+                baseSurname=r['baseSurname'],
+                givenName=r['givenName'],
+                surnamePrefix=r['surnamePrefix'],
+                disambiguatingDescription=r['disambiguatingDescription'],
+                literalName=" ".join([
+                    i for i in
+                    [r['givenName'], r['surnamePrefix'], r['baseSurname']]
+                    if i is not None
+                ]))
+        ]
+
+        if r['altName']:
+            personNames.append(PersonName(None, literalName=r['altName']))
+
+        for name in personNames:
+            name.label = name.literalName
+
+        personObservation = PersonObservation(
+            nsPersonObservation.term(str(next(personCounter))),
+            hasName=personNames,
+            documentedIn=document,
+            locationInDocument=Literal(r['folio']) if r['folio'] else None,
+            wasDerivedFrom=[annotation])
+
+        personObservation.label = [i.literalName for i in personNames]
+
+        if r['neighbourhood']:
+            locationObservation = LocationObservation(
+                nsLocationObservation.term(r['neighbourhood']),
+                label=[r['neighbourhood']])
+
+            personObservation.hasLocation = [locationObservation]
+
+    DERIVATION.entity = sorted(images)
+
+    return g
+
+
+if __name__ == "__main__":
+    g = main('data/records.csv')
+
+    print("Serializing!")
+    g.serialize('data/records.trig', format='trig')
---
+++ roar.py
@@ -0,0 +1,380 @@
+"""
+SAA ontology to be used in direct conversion from SAA to Golden Agents. 
+"""
+
+from rdflib import Dataset, Graph, Namespace
+from rdflib import XSD, RDF, RDFS, OWL
+from rdflib import URIRef, BNode, Literal
+
+from rdfalchemy.rdfSubject import rdfSubject
+from rdfalchemy import rdfSingle, rdfMultiple, rdfContainer, rdfList
+
+bio = Namespace("http://purl.org/vocab/bio/0.1/")
+edm = Namespace("http://www.europeana.eu/schemas/edm/")
+sem = Namespace("http://semanticweb.cs.vu.nl/2009/11/sem/")
+rel = Namespace("http://purl.org/vocab/relationship/")
+pnv = Namespace('https://w3id.org/pnv#')
+skos = Namespace('http://www.w3.org/2004/02/skos/core#')
+schema = Namespace('http://schema.org/')
+dc = Namespace("http://purl.org/dc/elements/1.1/")
+dcterms = Namespace("http://purl.org/dc/terms/")
+
+AS = Namespace("http://www.w3.org/ns/activitystreams#")
+oa = Namespace("http://www.w3.org/ns/oa#")
+void = Namespace("https://www.w3.org/TR/void/")
+foaf = Namespace("http://xmlns.com/foaf/0.1/")
+prov = Namespace("http://www.w3.org/ns/prov#")
+roar = Namespace("https://w3id.org/roar#")
+
+
+
+PersonObservation = Namespace(
+    "https://data.create.humanities.uva.nl/datasets/kohier1674/observations/PersonObservation/"
+)
+OccupationObservation = Namespace(
+    "https://data.create.humanities.uva.nl/datasets/kohier1674/observations/OccupationObservation/"
+)
+
+# Void
+class VoidDataset(rdfSubject):
+    rdf_type = void.Dataset
+
+    subset = rdfMultiple(void.subset)
+
+    title = rdfSingle(dcterms.title)
+    description = rdfSingle(dcterms.description)
+
+    label = rdfMultiple(RDFS.label)
+
+
+# Prov
+
+class Entity(rdfSubject):
+    rdf_type = prov.Entity
+    
+    locationInDocument = rdfSingle(roar.locationInDocument)
+
+class Derivation(rdfSubject):
+    rdf_type = prov.Derivation
+
+    hadActivity = rdfSingle(prov.hadActivity, range_type=prov.Activity)
+    entity = rdfMultiple(prov.entity, range_type=prov.Entity)
+
+class Activity(rdfSubject):
+    rdf_type = prov.Activity
+
+    wasAssociatedWith = rdfSingle(prov.wasAssociatedWith,
+                                  range_type=prov.Agent)
+    qualifiedAssociation = rdfSingle(prov.qualifiedAssociation,
+                                     range_type=prov.Association)
+
+    comment = rdfSingle(RDFS.comment)
+
+class Association(rdfSubject):
+    rdf_type = prov.Association
+
+    hadPlan = rdfSingle(prov.hadPlan, range_type=prov.Plan)
+    agent = rdfSingle(prov.agent, range_type=prov.Agent)
+
+    comment = rdfSingle(RDFS.comment)
+
+
+class Plan(rdfSubject):
+    rdf_type = prov.Plan
+
+    comment = rdfSingle(RDFS.comment)
+
+
+class Agent(rdfSubject):
+    rdf_type = prov.Agent
+
+#############
+# SAA Index #
+#############
+
+
+class Entity(rdfSubject):
+    rdf_type = prov.Entity
+    label = rdfMultiple(RDFS.label)
+    comment = rdfMultiple(RDFS.comment)
+
+    wasDerivedFrom = rdfMultiple(prov.wasDerivedFrom)
+    qualifiedDerivation = rdfSingle(prov.qualifiedDerivation,
+                                    range_type=prov.Derivation)
+
+    inDataset = rdfSingle(void.inDataset)
+
+    hasLocation = rdfMultiple(roar.hasLocation)
+
+    documentedIn = rdfSingle(roar.documentedIn)
+
+    identifier = rdfSingle(schema.identifier)
+
+class ProvidedCHO(Entity):
+    rdf_type = edm.ProvidedCHO
+    
+class WebResource(Entity):
+    rdf_type = edm.WebResource
+    
+class Aggregation(Entity):
+    rdf_type = edm.Aggregation
+    
+    aggregatedCHO = rdfSingle(edm.aggregatedCHO)
+    
+    hasView = rdfSingle(edm.hasView)
+    isShownAt = rdfSingle(edm.isShownAt)
+    isShownBy = rdfSingle(edm.isShownBy)
+
+class Document(Entity):
+    rdf_type = roar.Document, edm.ProvidedCHO
+    identifier = rdfSingle(schema.identifier)
+
+    language = rdfSingle(schema.isInLanguage)
+
+    label = rdfMultiple(RDFS.label)
+
+    onScan = rdfMultiple(roar.onScan)
+    
+    items = rdfList(AS.items)  # rdf:Collection
+    prev = rdfSingle(AS.prev)
+    next = rdfSingle(AS.next)
+
+
+class Derivation(rdfSubject):
+    rdf_type = prov.Derivation
+    label = rdfMultiple(RDFS.label)
+    comment = rdfMultiple(RDFS.comment)
+
+    hadActivity = rdfSingle(prov.hadActivity)
+    entity = rdfMultiple(prov.entity)
+
+
+class Activity(rdfSubject):
+    rdf_type = prov.Activity
+    label = rdfMultiple(RDFS.label)
+    comment = rdfMultiple(RDFS.comment)
+
+    wasAssociatedWith = rdfMultiple(prov.wasAssociatedWith,
+                                    range_type=prov.Agent)
+    qualifiedAssociation = rdfSingle(prov.qualifiedAssociation,
+                                     range_type=prov.Association)
+
+
+class Agent(rdfSubject):
+    rdf_type = prov.Agent
+    label = rdfMultiple(RDFS.label)
+    comment = rdfMultiple(RDFS.comment)
+
+class StructuredValue(rdfSubject):
+    value = rdfSingle(RDF.value)
+
+    role = rdfSingle(roar.role)
+
+    hasTimeStamp = rdfSingle(sem.hasTimeStamp)
+    hasBeginTimeStamp = rdfSingle(sem.hasBeginTimeStamp)
+    hasEndTimeStamp = rdfSingle(sem.hasEndTimeStamp)
+    hasEarliestBeginTimeStamp = rdfSingle(sem.hasEarliestBeginTimeStamp)
+    hasLatestBeginTimeStamp = rdfSingle(sem.hasLatestBeginTimeStamp)
+    hasEarliestEndTimeStamp = rdfSingle(sem.hasEarliestEndTimeStamp)
+    hasLatestEndTimeStamp = rdfSingle(sem.hasLatestEndTimeStamp)
+
+    label = rdfMultiple(RDFS.label)
+
+
+##########
+# Person #
+##########
+
+
+class Person(Entity):
+    rdf_type = schema.Person
+
+    hasName = rdfMultiple(pnv.hasName, range_type=pnv.PersonName)  # resource
+
+    label = rdfMultiple(RDFS.label)
+
+    birth = rdfSingle(bio.birth)
+    death = rdfSingle(bio.death)
+    event = rdfMultiple(bio.event)
+
+    birthDate = rdfSingle(schema.birthDate)
+    birthPlace = rdfSingle(schema.birthPlace)
+    deathDate = rdfSingle(schema.deathDate)
+    deathPlace = rdfSingle(schema.deathPlace)
+
+    gender = rdfSingle(schema.gender)
+
+    mother = rdfSingle(bio.mother)
+    father = rdfSingle(bio.father)
+    child = rdfSingle(bio.child)
+
+    spouse = rdfMultiple(schema.spouse)
+    parent = rdfMultiple(schema.parent)
+    children = rdfMultiple(schema.children)
+
+    hasOccupation = rdfMultiple(schema.hasOccupation)
+
+    address = rdfSingle(schema.address)
+    homeLocation = rdfSingle(schema.homeLocation)
+
+
+class PersonObservation(Person):
+    rdf_type = roar.PersonObservation
+    
+    locationInDocument = rdfSingle(roar.locationInDocument)
+
+
+class PersonReconstruction(Person):
+    rdf_type = roar.PersonReconstruction
+
+
+class LocationObservation(Entity):
+    rdf_type = roar.LocationObservation
+
+    hasPerson = rdfMultiple(roar.hasPerson)
+    address = rdfSingle(schema.address)
+
+    geoWithin = rdfSingle(schema.geoWithin)
+
+
+class LocationReconstruction(Entity):
+    rdf_type = roar.LocationReconstruction
+
+
+class PostalAddress(Entity):
+    rdf_type = schema.PostalAddress
+
+    streetAddress = rdfSingle(schema.streetAddress)
+    addressRegion = rdfSingle(schema.addressRegion)
+    postalCode = rdfSingle(schema.postalCode)
+    disambiguatingDescription = rdfSingle(schema.disambiguatingDescription)
+
+
+class PersonName(rdfSubject):
+    rdf_type = pnv.PersonName
+    label = rdfSingle(RDFS.label)
+
+    # These map to A2A
+    literalName = rdfSingle(pnv.literalName)
+    givenName = rdfSingle(pnv.givenName)
+    surnamePrefix = rdfSingle(pnv.surnamePrefix)
+    baseSurname = rdfSingle(pnv.baseSurname)
+
+    # These do not
+    prefix = rdfSingle(pnv.prefix)
+    disambiguatingDescription = rdfSingle(pnv.disambiguatingDescription)
+    patronym = rdfSingle(pnv.patronym)
+    surname = rdfSingle(pnv.surname)
+
+    nameSpecification = rdfSingle(pnv.nameSpecification)
+
+
+
+class Occupation(rdfSubject):
+    rdf_type = schema.Occupation
+    label = rdfMultiple(RDFS.label)
+    name = rdfMultiple(schema.name)
+
+    occupationalCategory = rdfMultiple(schema.occupationalCategory,
+                                       range_type=schema.CategoryCode)
+
+
+class OccupationObservation(Entity):
+    rdf_type = roar.OccupationObservation
+    label = rdfMultiple(RDFS.label)
+    name = rdfMultiple(schema.name)
+
+    occupationalCategory = rdfMultiple(schema.occupationalCategory,
+                                       range_type=schema.CategoryCode)
+
+
+class CategoryCode(rdfSubject):
+    rdf_type = schema.CategoryCode
+    inCodeSet = rdfSingle(schema.inCodeSet, range_type=schema.CategoryCodeSet)
+    codeValue = rdfSingle(schema.codeValue)
+
+    label = rdfMultiple(RDFS.label)
+    name = rdfMultiple(schema.name)
+
+
+class CategoryCodeSet(rdfSubject):
+    rdf_type = schema.CategoryCodeSet
+    label = rdfMultiple(RDFS.label)
+    name = rdfMultiple(schema.name)
+
+    # {
+    #     "@context": "http://schema.org/",
+    #     "@type": "Occupation",
+    #     "name": "Film actor",
+    #     "occupationalCategory": {
+    #         "@type": "CategoryCode",
+    #         "inCodeSet": {
+    #             "@type": "CategoryCodeSet",
+    #             "name": "HISCO"
+    #         },
+    #         "codeValue": "17320",
+    #         "name": "Actor"
+    #     }
+    # }
+
+
+class Annotation(Entity):
+    rdf_type = oa.Annotation
+    hasTarget = rdfSingle(oa.hasTarget)
+
+    bodyValue = rdfSingle(oa.bodyValue)
+
+    hasBody = rdfSingle(oa.hasBody)  # or multiple?
+
+    motivatedBy = rdfSingle(oa.motivatedBy)
+
+    depiction = rdfSingle(foaf.depiction)
+
+
+class SpecificResource(Entity):
+    rdf_type = oa.SpecificResource
+
+    hasSource = rdfSingle(oa.hasSource)
+    hasSelector = rdfSingle(oa.hasSelector)
+    hasState = rdfSingle(oa.hasState)
+    hasPurpose = rdfSingle(oa.hasPurpose)
+
+
+class Selector(Entity):
+    rdf_type = oa.Selector
+
+
+class FragmentSelector(Entity):
+    rdf_type = oa.FragmentSelector
+
+    conformsTo = rdfSingle(dcterms.conformsTo)
+    value = rdfSingle(RDF.value)
+
+
+class TextQuoteSelector(Entity):
+    rdf_type = oa.TextQuoteSelector
+
+
+class TextPositionSelector(Entity):
+    rdf_type = oa.TextPositionSelector
+    
+class ResourceSelection(Entity):
+    rdf_type = None
+
+    hasSource = rdfSingle(oa.hasSource)
+    hasSelector = rdfSingle(oa.hasSelector)
+    hasState = rdfSingle(oa.hasState)
+
+
+class FragmentSelector(Entity):
+    rdf_type = oa.FragmentSelector
+
+    conformsTo = rdfSingle(dcterms.conformsTo)
+    value = rdfSingle(RDF.value)
+    
+class TextualBody(Entity):
+    rdf_type = oa.TextualBody
+    
+    value=rdfSingle(RDF.value)
+    language=rdfSingle(dc.language)
+    format=rdfSingle(dc.term('format'))
